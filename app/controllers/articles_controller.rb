d0f73b70e9130ce41812c52cc1ed2c6b40cc6c4a
Nick Grey <soundsnick@gmail.com>
2018-03-13 18:20:11 +0600

    Tried to make some changes in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,34 +2,38 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all
 	end
+
+	def new
+	end
+	
 	def show
 		@article = Article.find(params[:id])
 	end
-	def new
-
-	end
+	
 	def create
 		@article = Article.new(params.require(:article).permit(:title, :text))
- 
 		@article.save
 		redirect_to articles_path, notice: "Succesfully created"
 	end
+
 	def edit
 		id = params[:id]
         @article = Article.find(id)
-        render :edit
 	end
+
 	def destroy
 		@article = Article.destroy(params[:id])
 		redirect_to articles_path, notice: "Succesfully deleted"
 	end
+
 	def update
 		@article = Article.find(params[:id])
   		@article.update_attributes(params.require(:article).permit(:title, :text))
   		redirect_to articles_path, notice: "Article updated"
 	end
+	
 	private
-	  def article_params
-	    params.require(:article).permit(:title, :text)
-	  end
+	def article_params
+	  params.require(:article).permit(:title, :text)
+	end
 end
