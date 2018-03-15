c1f1c10979d6d68fdcf9769fe38901ca9e18a6ee
Nick Grey <soundsnick@gmail.com>
2018-03-15 14:15:32 +0600

    Deleted .idea folder

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   def self.search(search)
     if search
-      where("title LIKE ?", "%#{search}%")
+      where("LOWER(title) LIKE ?", "%#{search.downcase}%")
     else
       all
     end
